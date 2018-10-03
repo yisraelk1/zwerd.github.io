9666f7361989a70fb8dc5ae52c84a26cb3df7f86
GuyZwer <guy.zwerdling@gmail.com>
2018-10-03 15:02:07 +0300

    update project0

--- projects/project0/script.js
+++ projects/project0/script.js
@@ -8,34 +8,66 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
-const itemList = []
-let checkedNumber = parseInt(0)
-let item,itemsNumber;
-
+let todoName = 'task';
+let todoUncheck = []
+let todoList = []
+let todoText = String()
+let newelement = ''
+let todoNumber = 0
 
 function newTodo() {
-  item = prompt("Enter TODO for your list: ")
-  itemList.push(item)
-  itemsNumber = parseInt(itemList.length)
-  list.innerHTML +=  '<input type="checkbox" id="'+item+'"name="TODO" value="'+item+'"onClick="toggleCheckbox(this)">'+item+"<button id='"+item+"' onclick='removeItem("+item+")'>Delete</button><br>"
-  console.log(itemsNumber)
-  itemCountSpan.innerHTML = itemsNumber
-  uncheckedCountSpan.innerHTML = itemsNumber - checkedNumber
-  console.log(classNames.TODO_ITEM)
+  todoNumber += 1;
+  todoTask = todoName + todoNumber;
+  todoList.push(todoTask);
+  todoUncheck.push(todoTask);
+  todoText = callPrompt()
+  newelement = '<button id="delete'+todoTask+'" onclick="remove(this)">Delete</button>'
+               +'<li id="'+todoTask+'" onclick="changeColor(this)">'+todoText+'</li>'
+  itemCountSpan.innerHTML = todoList.length
+  uncheckedCountSpan.innerHTML = todoUncheck.length
+  list.innerHTML += newelement
+  document.getElementById(todoTask).style.backgroundColor = 'red'
 }
-function toggleCheckbox(element) {
-  if(element.checked){
-    checkedNumber += parseInt(1)
-    console.log(checkedNumber)
+
+function remove(element){
+  let id = element.id.slice(6, element.id.langth)
+  list.removeChild(element)
+  list.removeChild(document.getElementById(id))
+  removeA(todoList, id)
+  removeA(todoUncheck, id)
+  itemCountSpan.innerHTML = todoList.length
+  uncheckedCountSpan.innerHTML = todoUncheck.length
+}
+
+function changeColor(element){
+  if(element.style.backgroundColor == 'red'){
+    element.style.backgroundColor = 'green'
+    console.log(todoUncheck.indexOf(element.id))
+    removeA(todoUncheck, element.id)
+    uncheckedCountSpan.innerHTML = todoUncheck.length
   }else{
-    checkedNumber -= parseInt(1)
+    element.style.backgroundColor = 'red'
+    todoUncheck.push(element.id)
+    uncheckedCountSpan.innerHTML = todoUncheck.length
   }
-  uncheckedCountSpan.innerHTML = itemsNumber - checkedNumber
-  console.log(itemsNumber - checkedNumber, itemsNumber,checkedNumber)
-  console.log(element.checked)
 }
-function removeItem(item){
-  var ul = document.getElementById("todo-list");
-  var element = document.getElementById(item);
-  ul.removeChild(element);
+
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
 }
+
+function callPrompt(){
+  task = prompt("Please enter your task in TODO list: ");
+  if(task == ''){
+    alert("Not valide task!")
+    callPrompt()
+  }
+ return task;
+ }
